ec0c8b6508056191ed52c1cd63c1cde17896ce28
tanggd <tanggd11@foxmail.com>
2021-07-01 11:40:38 +0800

    feat: 解决vue-tsc打包出现的错误

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+// import 'ant-design-vue/dist/antd.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
