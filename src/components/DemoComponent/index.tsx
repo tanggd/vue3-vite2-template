c8c01656c4ea6e9e784df65bb41e179704ec2b8c
tanggd <tanggd11@foxmail.com>
2021-07-09 18:27:58 +0800

    feat: 优化组件，增加vue-types

--- src/components/DemoComponent/index.tsx
+++ src/components/DemoComponent/index.tsx
@@ -1,19 +1,37 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import VueTypes from 'vue-types'
 import styles from './style/index.module.scss'
 
 const demoComponentProps = {
-  msg: {
-    type: String,
-    default: 'Hello Vue3-tsx',
-  },
+  // TODO：更改值，或新增加属性，页面热更新并未更新该值的问题
+  name: VueTypes.string.def('组件示例'),
+  msg: VueTypes.string,
+  id: VueTypes.number.isRequired,
 }
 
 const DemoComponent = defineComponent({
   name: 'DemoComponent',
   props: demoComponentProps,
   setup(props) {
+    const msg = computed(() => `Hello ${props.msg}`)
+
     return () => {
-      return <div class={[styles.border, styles.color]}>{props.msg}</div>
+      return (
+        <div class={[styles['border'], styles['border-red']]}>
+          <div>
+            <span>标题：</span>
+            <span>{props.name}</span>
+          </div>
+          <div>
+            <span>信息：</span>
+            <span>{msg.value}</span>
+          </div>
+          <div>
+            <span>id：</span>
+            <span>{props.id}</span>
+          </div>
+        </div>
+      )
     }
   },
 })
--- src/views/ComponentDemo/index.tsx
+++ src/views/ComponentDemo/index.tsx
@@ -1,13 +1,34 @@
 import DemoComponent from '@/components/DemoComponent'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'ComponentDemo',
   setup() {
-    return () => (
-      <div>
-        <DemoComponent msg="hello JS" />
-      </div>
-    )
+    const btns = ['JS', 'Java', 'Vue3']
+    const msg = ref('')
+    const changeMsg = (text) => {
+      msg.value = text
+    }
+
+    return () => {
+      return (
+        <div>
+          <a-space>
+            {btns.map((text) => (
+              <a-button
+                type={msg.value === text ? 'primary' : 'default'}
+                onClick={() => {
+                  changeMsg(text)
+                }}
+              >
+                {text}
+              </a-button>
+            ))}
+          </a-space>
+
+          <DemoComponent id={123} msg={msg.value} />
+        </div>
+      )
+    }
   },
 })
